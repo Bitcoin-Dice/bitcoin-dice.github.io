d9f2788bc9d8015528c0437f27f66c1bacd843b9
Bitcoin Dice <jasonapurvis@gmail.com>
2016-07-04 12:41:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1345,12 +1345,6 @@ var BetBoxWager = React.createClass({
     );
   }
 });
-var a = document.createElement('a');
-var linkText = document.createTextNode("my title text");
-a.appendChild(linkText);
-a.title = "my title text";
-a.href = "http://example.com";
-document.body.appendChild(a);
 
 var BetBoxButton = React.createClass({
   displayName: 'BetBoxButton',
